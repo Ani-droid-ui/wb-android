05072f164dfbac49e5ec5223858c711429d8e35d
🤍🩶💜Ani💜🩶🤍 <24yasina@dartfordgrammargirls.org.uk>
2025-10-05 12:43:15 +0100

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,68 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.example.widgetbloom"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.widgetbloom"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    // Core Android
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    
+    // Material Design
+    implementation("com.google.android.material:material:1.11.0")
+    
+    // ConstraintLayout
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    
+    // CardView
+    implementation("androidx.cardview:cardview:1.0.0")
+    
+    // Retrofit for API calls
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    
+    // Google Play Services Location
+    implementation("com.google.android.gms:play-services-location:21.1.0")
+    
+    // Activity Result API
+    implementation("androidx.activity:activity-ktx:1.8.2")
+    
+    // Testing
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
